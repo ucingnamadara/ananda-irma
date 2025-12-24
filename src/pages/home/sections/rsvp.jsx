24f942ab052416e8b6cb62baa972d48103dcf031
Dana <anandapratama03@gmail.com>
2025-12-25 00:42:50 +0700

    add animation

--- src/pages/home/sections/rsvp.jsx
+++ src/pages/home/sections/rsvp.jsx
@@ -4,30 +4,35 @@ import Button from '../../../components/button';
 import { useRvspList } from '../../../hooks/userRvspList';
 import { formatAttendanceStatus, formatDate } from '../../../utils/formatter';
 import { getAllRsvps, submitRsvp } from '../../../api/rsvpService';
+import Loading from "../../../components/loading";
 
 export default function RsvpSection() {
     const itemsPerPage = 5;
     const [rsvpList, setRsvpList] = useState([]);
-    const [formData, setFormData] = useState({ name: '', total: '1', isPresence: true, comment: '' });
+    const [formData, setFormData] = useState({ name: null, total: '1', isPresence: true, comment: null });
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
-    const {loading, error } = useRvspList({page: currentPage, limit: itemsPerPage, rsvpList: rsvpList, setRsvpList: setRsvpList, setTotalPages: setTotalPages, setCurrentPage: setCurrentPage});
-    
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    useRvspList({page: currentPage, limit: itemsPerPage, rsvpList: rsvpList, setRsvpList: setRsvpList, setTotalPages: setTotalPages, setCurrentPage: setCurrentPage});
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
-        const guestId = localStorage.getItem('guestId');
-        await submitRsvp({...formData, guestId});
-        setCurrentPage(1)
-        const response = await getAllRsvps({page: currentPage, limit: itemsPerPage});
-        if (response.status === 200) {
-            if(response.data.rsvps.length === 0){
-                setCurrentPage(prev => prev - 1);
-                return;
-            } else{
-                setRsvpList(response.data.rsvps);
+        if(!isSubmitted && formData.name !== null){
+            setIsSubmitted(true)
+            const guestId = localStorage.getItem('guestId');
+            await submitRsvp({...formData, guestId});
+            setCurrentPage(1)
+            const response = await getAllRsvps({page: currentPage, limit: itemsPerPage});
+            if (response.status === 200) {
+                if(response.data.rsvps.length === 0){
+                    setCurrentPage(prev => prev - 1);
+                    return;
+                } else{
+                    setRsvpList(response.data.rsvps);
+                }
             }
         }
+        
     };
 
     const handleShowMore = async () => {
@@ -49,14 +54,14 @@ export default function RsvpSection() {
         <div className="bg-surface-cream w-full flex flex-col justify-center items-center pt-20 px-4 -z-1 text-brand-dark text-[15px]">
             <img src="/assets/decorations/curl-line.svg" alt="Curl Decoration" className=" md:object-none z-0"/>
             <h2 data-animate className='text-[24px] font-semibold mt-10 opacity-0 transition-all duration-800'>RSVP & Blessing</h2>
-            <p className='mb-5 mt-2'>We would like to share this special moment with you. Please confirm your attendance below and share your blessings with us.</p>
+            <p data-animate className='mb-5 mt-2 opacity-0 transition-all duration-800'>We would like to share this special moment with you. Please confirm your attendance below and share your blessings with us.</p>
 
-            <form className="w-3/4 flex flex-col gap-5">
+            <form data-animate className="w-3/4 flex flex-col gap-5 opacity-0 transition-all duration-800">
                 <InputForm formData={formData} setFormData={setFormData}></InputForm>
                 <Button className ="text-surface-cream bg-brand-dark" label={"Submit RSVP"} onClick={handleFormSubmit}></Button>
             </form>
 
-            <div className="mt-15 flex flex-col gap-6 w-3/4">
+            <div data-animate className="mt-15 flex flex-col gap-6 w-3/4 opacity-0 transition-all duration-800">
             {
                 rsvpList?.map((rvsp) => {
                     return (
