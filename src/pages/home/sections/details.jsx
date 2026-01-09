9424f7b8b4d60f32723d140455890a458a1d1825
Dana <anandapratama03@gmail.com>
2026-01-09 20:36:05 +0700

    fix overflow

--- src/pages/home/sections/details.jsx
+++ src/pages/home/sections/details.jsx
@@ -24,18 +24,18 @@ function Details(){
                     <img src="/assets/decorations/flower-green.svg" className="rotate-20" style={{ transform: `rotate(${rotation}deg)` }}></img>
                 </div>
             </div>
-            <div className="z-1">
+            <div className="z-1 overflow-x-hidden">
                 <div className="flex flex-row p-5 gap-3">
-                    <div data-aos="fade-right" className="text-right flex flex-col justify-center items-end w-3/5 gap-2 left-animate ">
+                    <div data-aos="fade-right" className="text-right flex flex-col justify-center items-end w-3/5 gap-2 ">
                         <p className="font-playfair text-[40px] md:text-[45px] font-bold tracking-tight line-clamp-3 leading-10">Ananda <span className="font-normal">Dana Pratama</span></p>
                         <img src="/assets/decorations/line-orange.svg" alt="Line Divider" className=""></img>
                         <p className="text-[15px] text-forest-dark font-medium">Son of Mr. Eka Dana Pramulia Putra & Mrs. Susi Suryani Elia</p>
                     </div>
-                    <img data-aos="fade-left" src="/assets/images/grooms.svg" alt="Ananda Dana Pratama" className="w-2/5 right-animate "></img>
+                    <img data-aos="fade-left" src="/assets/images/grooms.svg" alt="Ananda Dana Pratama" className="w-2/5"></img>
                 </div>
                 <div className="flex flex-row p-5 gap-3">
-                    <img data-aos="fade-right" src="/assets/images/brides.svg" alt="Ananda Dana Pratama" className="w-2/5 left-animate "></img>
-                    <div data-aos="fade-left" className="text-left flex flex-col justify-center items-start w-3/5 gap-2 right-animate ">
+                    <img data-aos="fade-right" src="/assets/images/brides.svg" alt="Ananda Dana Pratama" className="w-2/5"></img>
+                    <div data-aos="fade-left" className="text-left flex flex-col justify-center items-start w-3/5 gap-2">
                         <p className="font-playfair text-[40px] md:text-[45px] font-bold tracking-tight line-clamp-3 leading-10">Irmayanti <span className="font-normal">Nur Sabillah</span></p>
                         <img src="/assets/decorations/line-orange.svg" alt="Line Divider" className=""></img>
                         <p className="text-[15px] text-forest-dark font-medium">Daughter of Mr. Ir. Iman Irnaman & Mrs. Suhaeti</p>
--- src/pages/home/sections/wedding-gift.jsx
+++ src/pages/home/sections/wedding-gift.jsx
@@ -4,8 +4,8 @@ function WeddingGift(){
         <img data-aos="fade-in" src="/assets/decorations/curl-line.svg" alt="Curl Decoration" className=" md:object-none z-0"/>
         <h1 data-aos="fade-up" className="text-[24px] font-semibold mt-10 ">Wedding Gift</h1>
         <p data-aos="fade-up" className="mt-2 ">We are truly grateful to share this moment with you. Your presence is more than enough and already means so much to us. Should you wish to give a gift, it will be received with heartfelt gratitude.</p>
-        <div className="w-full">
-            <div data-aos="fade-up" className="flex flex-row gap-2 md:gap-5 mt-20 mb-30 justify-center items-center">
+        <div className="w-full overflow-x-hidden">
+            <div data-aos="fade-left" className="flex flex-row gap-2 md:gap-5 mt-20 mb-30 justify-center items-center">
                 <img src="/assets/decorations/bank-account-frame.svg" className="absolute max-w-[397px] w-23/24"></img>
                 <img src="/assets/images/mandiri-logo.svg" alt="Gift Box"></img>
                 <div className="text-left flex flex-col gap-0.5">
@@ -13,7 +13,7 @@ function WeddingGift(){
                     <p>Ananda Dana Pratama</p>
                 </div>
             </div>
-            <div data-aos="fade-up" className="flex flex-row gap-5 my-25 justify-center items-center ">
+            <div data-aos="fade-right" className="flex flex-row gap-5 my-25 justify-center items-center ">
                 <img src="/assets/decorations/bank-account-frame.svg" className="absolute max-w-[397px] w-23/24"></img>
                 <img src="/assets/images/bca-logo.svg" alt="Gift Box"></img>
                 <div className="text-left flex flex-col gap-0.5">
